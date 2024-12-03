e5a4fa5909a42aa38c4845f1ad62950df9f25522
Owen Hancock <owenh@qualtrics.com>
2024-12-03 14:49:17 -0800

    Example with the new `remoteBasePath` feature.

--- packages/host/rsbuild.config.ts
+++ packages/host/rsbuild.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
 				'buttons': 'buttons@/module/buttons/mf-manifest.json'
 			},
 			shared: ['react', 'react-dom'],
+			dts: {
+				consumeTypes: {
+					remoteBasePath: 'http://localhost:3001/module',
+				}
+			}
 		}),
 	],
 });
